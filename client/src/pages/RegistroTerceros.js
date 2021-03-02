4525c5c372ed3c39b01fc6068b479fe226c55013
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2021-03-02 13:04:35 -0500

    Header listo

---
+++ client/src/pages/RegistroTerceros.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Header from '../components/base/header'
+
+const RegistroTerceros = () => {
+    return (
+        <div className="RegistroTerceros">
+            <Header />
+        </div>
+    )
+}
+
+export default RegistroTerceros;
