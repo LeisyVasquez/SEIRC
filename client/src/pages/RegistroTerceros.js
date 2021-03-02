fff89f5b44ca2741b9b027930669037dee069e05
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2021-03-02 13:36:32 -0500

    Registro terceros creado

--- client/src/pages/RegistroTerceros.js
+++ client/src/pages/RegistroTerceros.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from '../components/base/header'
 import Footer from '../components/base/footer'
+import RegistroTercerosBody from '../components/registroTercerosBody'
 
 const RegistroTerceros = () => {
     return (
         <div className="RegistroTerceros">
             <Header />
+            <RegistroTercerosBody />
             <Footer />
         </div>
     )
