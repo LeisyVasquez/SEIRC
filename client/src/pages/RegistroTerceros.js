c4e35e6111055979f8e4ab484b0ceb7910c8cc9a
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2021-03-02 13:05:49 -0500

    Footer listo

--- client/src/pages/RegistroTerceros.js
+++ client/src/pages/RegistroTerceros.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from '../components/base/header'
+import Footer from '../components/base/footer'
 
 const RegistroTerceros = () => {
     return (
         <div className="RegistroTerceros">
             <Header />
+            <Footer />
         </div>
     )
 }
