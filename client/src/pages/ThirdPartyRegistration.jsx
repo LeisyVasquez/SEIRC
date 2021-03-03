23c73471f2ad94c7944c4441b0d13244373e398d
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2021-03-02 22:13:12 -0500

    Nombres cambiados a inglés.

---
+++ client/src/pages/ThirdPartyRegistration.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Header from '../components/base/header'
+import Footer from '../components/base/footer'
+import ThirdPartyRegistrationBody from '../components/thirdPartyRegistrationBody'
+
+const RegistroTerceros = () => {
+    return (
+        <div className="RegistroTerceros">
+            <Header />
+            <ThirdPartyRegistrationBody />
+            <Footer />
+        </div>
+    )
+}
+
+export default RegistroTerceros;
