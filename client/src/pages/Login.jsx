2385437f2133f90d5e46ea6d7ae5c5836eba572f
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2021-03-04 16:02:12 -0500

    Navbar añadido

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from '../components/base/header'
+import Navbar from '../components/base/navbar'
 import Footer from '../components/base/footer'
 import Login from '../components/login'
 
@@ -8,6 +9,7 @@ const login = () => {
     return (
         <div>
             <Header />
+            <Navbar />
             <Login />
             <Footer />
         </div>
