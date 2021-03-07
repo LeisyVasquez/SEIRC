97d31f4509e731f938132dfd5371be12774abce6
Juan Pablo Salazar Vásquez <juanpasinga@gmail.com>
2021-03-07 10:55:18 -0500

    Maximo en los input validados

--- client/src/components/adminRegistrationBody.jsx
+++ client/src/components/adminRegistrationBody.jsx
@@ -71,14 +71,14 @@ const AdminRegistrationBody = () => {
             <Container className="text-center mt-2 mx-auto my-5 p-5 bosy w-50" >
                 <h2>Registro de administradores</h2>
                 <form className="form-signin mt-5" id="form">
-                    <input type="text" id="name" className="form-control mb-3" placeholder="Nombre del usuario" onChange={data} />
+                    <input type="text" id="name" className="form-control mb-3" placeholder="Nombre del usuario" maxlength="50" onChange={data} />
 
-                    <input type="number" id="phone" className="form-control mb-3" placeholder="Teléfono" onChange={data} />
+                    <input type="number" id="phone" className="form-control mb-3" placeholder="Teléfono" maxlength="10" onChange={data} />
 
-                    <input type="text" id="direction" className="form-control mb-3" placeholder="Dirección" onChange={data} />
-                    
+                    <input type="text" id="direction" className="form-control mb-3" placeholder="Dirección" maxlength="100" onChange={data} />
+
                     <div class="input-group">
-                        <input type="text" id="userName" className="form-control mb-3 mr-3" placeholder="Nombre de usuario" onChange={data} />
+                        <input type="text" id="userName" className="form-control mb-3 mr-3" placeholder="Nombre de usuario" maxlength="50" onChange={data} />
                         <input type="text" id="password" className="form-control " placeholder="Contraseña" onChange={data} />
                     </div>
                     <ButtonCancel />
